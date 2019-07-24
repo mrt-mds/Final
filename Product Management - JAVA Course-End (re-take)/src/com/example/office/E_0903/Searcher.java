b4b165c2ea248932df7833cb571cd48bae992673
mrt-mds <33335670+mrt-mds@users.noreply.github.com>
2019-07-24 18:34:55 +0800

    Add files via upload

---
+++ Product Management - JAVA Course-End (re-take)/src/com/example/office/E_0903/Searcher.java
@@ -0,0 +1,31 @@
+package com.example.office.E_0903;
+
+//slide 256 and 257 and 258
+public class Searcher {
+	Warehouse warehouse;
+
+	public Warehouse(Warehouse warehouse){
+		
+	}
+
+	public void queryItem(int code) {
+
+	}
+
+	public abstract findItemByCode(int code){
+		return item;
+	}
+
+	public abstract void printDetail(Item item){
+		
+	}
+
+	public abstract void printErrorMessage(){
+		
+	}
+
+	public Searcher(Warehouse){
+		
+	}
+	
+}
